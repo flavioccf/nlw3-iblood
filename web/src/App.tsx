f4329115607a61ceec27e573739824c1036a0989
FLAVIO FILHO/LGESP Call Center TBT Part(flavio.filho@lge.com) <flavio.filho@lge.com>
2020-10-12 22:36:06 -0300

    first commit

---
+++ web/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './styles/global.css';
+import Landing from './pages/Landing';
+
+function App() {
+  return (
+    <Landing/>
+  );
+}
+
+export default App;
---
+++ web/src/pages/Landing/index.tsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import { PageLanding } from './styles';
+
+function Landing() {
+    return (
+        <PageLanding>
+
+        </PageLanding>
+    )
+}
+
+export default Landing;
